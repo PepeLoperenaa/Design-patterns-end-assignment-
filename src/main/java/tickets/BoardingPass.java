b3d163f84df6189fa0803ef0719cb5efacbcc81f
redcarla <37105797+redcarla@users.noreply.github.com>
2022-03-27 22:33:09 +0200

    Fill classes

--- src/main/java/tickets/BoardingPass.java
+++ src/main/java/tickets/BoardingPass.java
@@ -1,4 +1,35 @@
 package tickets;
 
+import java.sql.Time;
+
 public class BoardingPass {
+
+    public Time boardingTime;
+    private String gate;
+    private String seat;
+    private Ticket ticket;
+    public Passenger passenger;
+
+    public BoardingPass(String gate, String seat, Time boardingTime) {
+        this.gate = gate;
+        this.seat = seat;
+        this.boardingTime = boardingTime;
+
+    }
+
+    public String getGate() {
+        return gate;
+    }
+
+    public String getSeat() {
+        return seat;
+    }
+
+    public int getTicketNum() {
+        return ticket.getTicketNum();
+    }
+
+
 }
+
+
--- src/main/java/tickets/Passenger.java
+++ src/main/java/tickets/Passenger.java
@@ -1,4 +1,25 @@
 package tickets;
 
+import java.util.Date;
+
 public class Passenger {
+    public String firstName;
+    public String lastName;
+    public Date dateOfBirth;
+    public Boolean gender;
+
+    public Passenger(String firstName, String lastName, Date dateOfBirth, Boolean gender){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+    }
+
+    public String getFirstname(){return firstName;}
+    public String getLastName(){return lastName;}
+    public Date getDoB(){return dateOfBirth;}
+    public Boolean getGender(){return gender;}
+
+
+
 }
